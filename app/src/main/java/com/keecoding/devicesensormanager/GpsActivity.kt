b9c427e58fc12706e2cc0ae359d8f9ca7544f647
riskiilyas <riskiilyas03@gmail.com>
2022-08-08 20:09:35 +0700

    GPS Progress

--- app/src/main/java/com/keecoding/devicesensormanager/GpsActivity.kt
+++ app/src/main/java/com/keecoding/devicesensormanager/GpsActivity.kt
@@ -2,8 +2,13 @@ package com.keecoding.devicesensormanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.keecoding.devicesensormanager.databinding.ActivityGpsBinding
 
 class GpsActivity : AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var binding: ActivityGpsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gps)
